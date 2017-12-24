a3de3883378dec0dcc3197d73ad7306d6524faf1
Fathi Lakhdar <fathi@isamm.online>
2017-12-24 20:04:18 +0100

    update scroll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 var options={
   start: "09:00 pm",
-  end: "12:00 pm",
+  end: "00:00 am",
   duration: "00:15",
-  startEvery: "00:05",
-  max:60
+  startEvery: "00:05"
 }
 
 timepicker = new Timepicker('timepicker', options);
